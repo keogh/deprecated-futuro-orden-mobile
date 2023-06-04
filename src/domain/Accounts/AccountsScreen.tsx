66264ce54a7d6dbc0d97a42324e77d057b3e118b
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2023-06-04 10:22:29 -0700

    Add Router

---
+++ src/domain/Accounts/AccountsScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {ScrollView, Text} from 'react-native';
+
+export default function AccountsScreen() {
+  return (
+    <ScrollView>
+      <Text>Accounts</Text>
+    </ScrollView>
+  );
+}
