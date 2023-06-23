7514fdfbe53d3e7d5993cc2f07d22dfe04ec3c8e
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2023-06-23 09:31:12 -0700

    Add icons to tab nav bar and icons support to the app

--- src/domain/Navigator/Navigator.tsx
+++ src/domain/Navigator/Navigator.tsx
@@ -4,7 +4,11 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import LoginScreen from '../Login/LoginScreen';
 import AccountsScreen from '../Accounts/AccountsScreen';
-import { AccountsStackParamList, AppTabParamList, RootStackParamsList } from './types';
+import {
+  AccountsStackParamList,
+  AppTabParamList,
+  RootStackParamsList,
+} from './types';
 import {
   ACCOUNT_DETAILS,
   ACCOUNTS,
@@ -18,6 +22,7 @@ import DashboardScreen from '../Dashboard/DashboardScreen';
 import { AuthContext } from '../Auth/AuthProvider';
 import LogoutScreen from '../Auth/LogoutScreen';
 import AccountDetailsScreen from '../Accounts/AccountDetailsScreen';
+import { Icon } from '@rneui/themed';
 
 const Tab = createBottomTabNavigator<AppTabParamList>();
 const Stack = createNativeStackNavigator<RootStackParamsList>();
@@ -41,18 +46,41 @@ export default function Navigator() {
 const ACCOUNTS_STACK_SCREEN_OPTIONS = {
   headerShown: false,
   tabBarLabel: 'Accounts',
+  tabBarIcon: ({ color }: { color: string }) => (
+    <Icon name="account-balance-wallet" color={color} />
+  ),
+};
+
+const DASHBOARD_TAB_OPTIONS = {
+  tabBarIcon: ({ color }: { color: string }) => (
+    <Icon name="dashboard" color={color} />
+  ),
+};
+
+const LOGOUT_TAB_OPTIONS = {
+  tabBarIcon: ({ color }: { color: string }) => (
+    <Icon name="logout" color={color} />
+  ),
 };
 
 function AppNavigator() {
   return (
     <Tab.Navigator initialRouteName={DASHBOARD}>
-      <Tab.Screen name={DASHBOARD} component={DashboardScreen} />
+      <Tab.Screen
+        name={DASHBOARD}
+        component={DashboardScreen}
+        options={DASHBOARD_TAB_OPTIONS}
+      />
       <Tab.Screen
         name={ACCOUNTS_STACK}
         component={AccountsStack}
         options={ACCOUNTS_STACK_SCREEN_OPTIONS}
       />
-      <Tab.Screen name={LOGOUT} component={LogoutScreen} />
+      <Tab.Screen
+        name={LOGOUT}
+        component={LogoutScreen}
+        options={LOGOUT_TAB_OPTIONS}
+      />
     </Tab.Navigator>
   );
 }
